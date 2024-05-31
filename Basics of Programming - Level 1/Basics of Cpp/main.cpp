99854b1e6ebc1142cf4d3867c2a38003f0f92bd6
tech-saurabh382 <saurabhgawali3008@gmail.com>
2024-05-31 11:39:52 +0530

    Adding Basics C++ Programs

---
+++ Basics of Programming - Level 1/Basics of Cpp/main.cpp
@@ -0,0 +1,84 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    char ch;
+    cin >> ch;
+    cout << "here: " << ch << endl;
+    
+    // int marks;
+
+    // cout << "Enter the marks here: " << endl;
+    // // taking input from user
+    // cin >> marks;
+
+    // cout << "Printing marks: " << marks << endl;
+
+    // int age = 2;
+    // int car = 12;
+
+    // if (age >= 18 && car >= 1)
+    // {
+    //     cout << "Licence miljaega" << endl;
+    // }
+
+    // if (age >= 18 || car >= 1)
+    // {
+    //     cout << "Licence miljaega" << endl;
+    // }
+
+    // cout << !age << endl;
+
+    // int a = 20;
+    // int b = 10;
+
+    // cout << (a > b) << endl;
+    // cout << (a < b) << endl;
+    // cout << (a <= b) << endl;
+    // cout << (a >= b) << endl;
+    // cout << (a != b) << endl;
+    // cout << (a == b) << endl;
+
+    // int a = 5;
+    // int b = 10;
+
+    // cout << a + b << endl;
+    // cout << a - b << endl;
+    // cout << a * b << endl;
+    // cout << b / a << endl;
+    // cout << b % a << endl;
+
+    // int num;
+    // num = 12;
+    // cout << num << endl;
+
+    // // int -> 4 byte
+    // int num = 50;
+    // cout << num << endl;
+
+    // cout << sizeof(num) << endl;
+
+    // // character -> 1 byte
+    // char ch = 'k';
+    // cout << ch << endl;
+    // cout << sizeof(ch) << endl;
+
+    // // float -> 4 byte
+    // float point = 1.69;
+    // cout<< point << endl;
+    // cout<< sizeof(point) << endl;
+
+    // // long -> 4 byte
+    // long number = 23;
+    // cout<< number << endl;
+    // cout<< sizeof(number) << endl;
+
+    // cout << "Love " << endl << "babbar" << '\n';
+    // cout << "Saurabh Gawali" << endl;
+    // cout << endl;
+    // cout << endl;
+    // cout << '\n';
+    // cout << "Coder Saurabh";
+    return 0;
+}
